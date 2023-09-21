d7cfe6f44693ce9a22741adae8424913d863769a
Ivan <ivan.mondragon.sotres@gmail.com>
2023-09-21 04:21:19 -0600

    (ADD) Added controller to employee

---
+++ src/main/java/com/example/rest/RestEmployeeController.java
@@ -0,0 +1,32 @@
+package com.example.rest;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.CrudApplication;
+import com.example.model.Employee;
+import com.example.service.IEmployeeService;
+
+@RestController
+public class RestEmployeeController {
+
+	private static Logger LOG = LoggerFactory.getLogger(CrudApplication.class);
+	
+	@Autowired
+	IEmployeeService employeeService;
+	
+	@GetMapping
+	public Employee list(){
+		System.out.println("Si entra a listar");
+		LOG.info("Si entra a metodo");
+		employeeService.get();	
+		Employee empl = new Employee("Juan",1,2);
+		return empl;	
+	}
+
+}
